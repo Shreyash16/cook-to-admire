ab3a6aa25a367ff1e6516c634ff1bdb4de4ef571
Shreyash16 <32540618+Shreyash16@users.noreply.github.com>
2025-08-25 00:58:01 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '2rem' }}>
+      <h1>Cook to Admire</h1>
+      <p>Welcome to your Indian recipes app!</p>
+    </div>
+  )
+}
